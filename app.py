b4c4530c7d9cb6724e19b90f9dc7ae2eafdd9d1d
joyce20-meet <joyce20-meet@meet.mit.edu>
2019-11-28 08:25:19 -0500

    database

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ from flask import Flask, request, redirect, url_for, render_template
 
 app = Flask(__name__)
 @app.route('/')
-
-
 def home():
 	return render_template('home.html')
+
+@app.route('/about')
 def about():
 	return render_template('about.html')
+@app.route('/store')
 def store():
 	return render_template('store.html')
+@app.route('/cart')
 def cart():
 	return render_template('cart.html')
+@app.route('/login')
 def login():
 	return render_template('login.html')
 
--- database.py
+++ database.py
@@ -15,12 +15,12 @@ def add_product(ID,name,price,Picturelink,Description):
 		ID=ID,
 		name=name,
 		price=price,
-		PictureLink=PictureLink,
+		Picturelink=Picturelink,
 		Description=Description)
 	session.add(product_objection)
 	session.commit()
 
-add_product(1,"Family Charm",60,"https://www.google.ps/search?q=family+pandora+charm&client=ubuntu&hs=r9p&tbm=isch&source=iu&ictx=1&fir=5Tv-1k3aoh31KM%253A%252Cqc-eobWVNlK1qM%252C_&vet=1&usg=AI4_-kRxVsjSG_vc9w_P18mS19hZgEQ92w&sa=X&ved=2ahUKEwi_-aON4v3lAhXVxcQBHSrPCKMQ9QEwAnoECAoQDA#imgrc=5Tv-1k3aoh31KM:","Silver Charm ")
+add_product(1,"Family Charm",60,"none","Silver Charm ")
 def update_price(ID,price):
  
 	product_object = session.query(
@@ -46,8 +46,7 @@ def query_by_id(their_ID):
  	product= session.query(
       Product).filter_by(
        ID=their_ID).first()
-   return product
-
+  
 
 
 
